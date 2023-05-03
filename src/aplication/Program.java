bd059a020d613b0bcbcda6917767ff4b91fad0ea
Wallesson <wallessonfernando@gmail.com>
2023-05-03 14:56:16 -0300

    colocou board e piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,12 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position pos = new Position();
+		Board board = new Board(8, 8);
 
 	}
 
